0ae61a4401736a36fe201fce0f3de406e6c8f0bd
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 12:14:00 +0200

    capitulo 5

---
+++ Ejercicio4.java
@@ -0,0 +1,9 @@
+/* Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+un bucle for. */
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
