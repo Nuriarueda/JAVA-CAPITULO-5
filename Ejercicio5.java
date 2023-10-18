3dd7b723f6a210ff911481964b2a11f11124e295
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 12:33:34 +0200

    capitulo 5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -3,5 +3,10 @@ un bucle while. */
 public class Ejercicio5 {
     public static void main(String[] args) {
         
+        int i = 320;
+        while (i>=160) {
+            System.out.print(" " + i);
+            i-=20;
+        }
     }
 }
