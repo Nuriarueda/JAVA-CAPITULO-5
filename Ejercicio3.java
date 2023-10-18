1d83cce4225d5e3e5ef3025d9d5159a086a7e28e
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 12:25:21 +0200

    capitulo 5

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,8 +1,11 @@
 /* Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while. */
-import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.close();
+
+        int i = 0;
+        do {
+            System.out.print(" " + i);
+            i += 5;
+        } while (i<=100);
     }
 }
