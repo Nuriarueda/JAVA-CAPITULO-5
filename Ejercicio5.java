bef5586da349c79c71b811f98cd44aac67af9bd5
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 12:30:58 +0200

    capitulo 5

---
+++ Ejercicio5.java
@@ -0,0 +1,7 @@
+/* Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+un bucle while. */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+    }
+}
