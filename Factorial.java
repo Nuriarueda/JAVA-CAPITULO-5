1436c49dd06c60d519bc3ef563bf6a67cf5ef067
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-19 09:58:41 +0200

    capitulo 5

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Factorial {
+    
+    public static long factorial(int num){
+        long f = 1;
+        for (int i = 1; i<=num; i++){
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un numero: ");
+        int num = sc.nextInt();
+        long f = factorial(num);
+
+        System.out.println("El factorial de " + num + " es " + factorial(num));
+        sc.close();
+    }
+
+
+
+}
