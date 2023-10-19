986ecd5af1da106a8bebee9271e1ceac19e0fa31
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-19 09:37:12 +0200

    capitulo 5

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Ejercicio9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
         
         sc.close();
     }
