4512b7bcf6624e9bd60e30547d1fac2a9809e33e
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-25 12:10:49 +0200

    capitulo 5

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -5,7 +5,25 @@ public class Ejercicio13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        
+        System.out.println("Introduce 10 numeros");
+
+        int positivos = 0;
+        int negativos = 0;
+
+        for (int i=0; i<=10; i++){
+            System.out.print("Introduce numero: ");
+            int num = Integer.parseInt(sc.nextLine());
+
+            if (num>=0) {
+                positivos++;
+            } else {
+                negativos++;
+            }
+        }
+
+        System.out.println("Tienes " + positivos + " positivos y " + negativos + " negativos");
+
+
         sc.close();
     }
 }
