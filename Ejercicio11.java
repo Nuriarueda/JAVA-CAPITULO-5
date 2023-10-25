0708b26d9cbf69953af696206807962fff0e71dc
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-25 10:41:29 +0200

    capitulo 5

---
+++ Ejercicio11.java
@@ -0,0 +1,10 @@
+/* Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
+los 5 primeros números enteros a partir de uno que se introduce por teclado. */
+import java.util.Scanner;
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        sc.close();
+    }
+}
