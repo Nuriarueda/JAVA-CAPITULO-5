a05fab47a2018332a9b04d2fe730b6736ce32822
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-25 10:43:45 +0200

    capitulo 5

---
+++ Ejercicio13.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que lea una lista de diez números y determine cuántos
+son positivos, y cuántos son negativos. */
+import java.util.Scanner;
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
