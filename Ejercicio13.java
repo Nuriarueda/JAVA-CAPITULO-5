3d07add514c2b47d7e75fb8b2465fa7974fc677e
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-25 12:03:32 +0200

    capitulo 5

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Ejercicio13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        
         sc.close();
     }
 }
---
+++ Ejercicio14.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que pida una base y un exponente (entero positivo) y que
+calcule la potencia. */
+import java.util.Scanner;
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
