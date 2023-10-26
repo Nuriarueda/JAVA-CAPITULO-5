b5b3c33ad747a94d5800285e43b15d7c49b1acab
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 09:53:24 +0200

    capitulo 5

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Ejercicio20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Introduce la altura de la piramide por teclado: ");
+        int alt = Integer.parseInt(sc.nextLine());
+        System.out.print("Introduce el caracter con el que se pintara la piramide: ");
+        String caracter = sc.nextLine();
+
         sc.close();
     }
 }
