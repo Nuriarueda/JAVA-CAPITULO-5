7a7e8eb6093c87e0e53be75dfb857e4389efacb7
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 09:50:46 +0200

    capitulo 5

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class Ejercicio18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero: ");
+        int num1 = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Introduce el segundo numero: ");
+        int num2 = Integer.parseInt(sc.nextLine());
+
+
+        if (num1==num2) {
+            System.out.println("Tienen que ser numeros distintos");
+        } else if ( num2<num1){
+            int n = num1;
+            num1=num2;
+            num2=n;
+            for (int i = num1; i<n; i+=7){
+                System.out.print(" " + i);
+            }
+        } else {
+            for (int i = num1; i<num2; i+=7){
+                System.out.print(" " + i);
+            }
+        }
+
+
         sc.close();
     }
 }
