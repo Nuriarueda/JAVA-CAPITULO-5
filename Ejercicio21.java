a5144daac3c480d23a3df83bb8b7b01408fbff41
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 10:07:26 +0200

    capitulo 5

--- Ejercicio21.java
+++ Ejercicio21.java
@@ -6,6 +6,24 @@ import java.util.Scanner;
 public class Ejercicio21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        double suma = 0;
+        int num = 0;
+        int numeroIntroducido = 0;
+
+        System.out.println("Vaya introduciendo numeros y cuando quiera parar introduzca uno negativo");
+        System.out.println(">");
+        
+        while (numeroIntroducido>=0) {
+            numeroIntroducido = Integer.parseInt(sc.nextLine());
+            num ++;
+        }
+        System.out.println("Has introducido " + (num-1) + " numeros");
+
+        if ((numeroIntroducido%2)==1) {
+        } else {
+            
+        }
+
         sc.close();
     }
 }
