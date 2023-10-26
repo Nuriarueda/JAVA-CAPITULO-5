c4a48be3b2034ac1c198264e8f3fd0b1fc9001a6
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 10:14:50 +0200

    capitulo 5

--- Ejercicio22.java
+++ Ejercicio22.java
@@ -1,8 +1,20 @@
 /* Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos. */
-import java.util.Scanner;
 public class Ejercicio22 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        sc.close();
+
+        boolean esPrimo = true;
+        for (int i = 2; i<=100; i++){
+            
+            esPrimo = true;
+            for (int a = 2; a<i; a++){
+                if (i%a==0) {
+                    esPrimo=false;
+                }
+            }
+
+            if (esPrimo) {
+                System.out.println(" " + i );
+            }
+        }
     }
 }
