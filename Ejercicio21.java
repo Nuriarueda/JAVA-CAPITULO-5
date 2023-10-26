76a439864ebc23cfed272ef91913f35353042271
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 09:37:09 +0200

    capitulo 5

---
+++ Ejercicio21.java
@@ -0,0 +1,11 @@
+/* Realiza un programa que vaya pidiendo números hasta que se introduzca un
+numero negativo y nos diga cuantos números se han introducido, la media de
+los impares y el mayor de los pares. El número negativo sólo se utiliza para
+indicar el final de la introducción de datos pero no se incluye en el cómputo. */
+import java.util.Scanner;
+public class Ejercicio21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
