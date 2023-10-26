ffe43d5adb9a9fe7f56916f6cee5b1be61b7e72a
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-26 09:35:51 +0200

    capitulo 5

---
+++ Ejercicio20.java
@@ -0,0 +1,8 @@
+/* Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca. */
+import java.util.Scanner;
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
