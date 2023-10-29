c4c71fd4505d20f0194e6d9d22d809be3c223585
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:05:18 +0100

    capitulo 5

---
+++ Ejercicio24.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que lea un número n e imprima una pirámide de números
+con n filas como en la siguiente figura: */
+import java.util.Scanner;
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
