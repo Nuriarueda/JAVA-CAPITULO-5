9a2b28723cc29d9cabd6d6b827083803530c4cbb
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 12:46:00 +0100

    capitulo 5

--- Ejercicio16.java
+++ Ejercicio16.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class Ejercicio16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in); 
+        System.out.print("Introduce un numero: ");
+        int n = sc.nextInt();
+
+        boolean esPrimo = true;
+
+        for (int i = 2; i<n; i++){
+            if (n%i==0) {
+                esPrimo=false;
+            }
+        }
+
+        if (esPrimo) {
+            System.out.println("El numero introducido es primo");
+        } else {
+            System.out.println("El numero introducido no es primo");
+        }
+
+        System.out.println();
+        
         sc.close();
     }
 }
