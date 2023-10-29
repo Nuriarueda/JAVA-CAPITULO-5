563cd8034739327f806efd9ea103ee5afd2466b3
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:16:04 +0100

    capitulo 5

--- Ejercicio25.java
+++ Ejercicio25.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class Ejercicio25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero entero: ");
+        int numeroIntroducido = Integer.parseInt(sc.nextLine());
+        int n  = numeroIntroducido;
+        int vuelta = 0;
+
+        while (n > 0) {
+            vuelta = (vuelta * 10) + (n % 10);
+            n /= 10;
+        }
+
+        System.out.println("El numero dado la vuelta es: " + vuelta);
+
+
         sc.close();
     }
 }
