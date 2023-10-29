fd69b0c750d5136713a6a3d09a6807dec6ccc71e
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:08:20 +0100

    capitulo 5

---
+++ Ejercicio25.java
@@ -0,0 +1,9 @@
+/* Realiza un programa que pida un número por teclado y que luego muestre ese
+número al revés. */
+import java.util.Scanner;
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
