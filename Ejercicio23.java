72d1f1527d943582d6a2ab2a43c905c6438fe9cf
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:03:47 +0100

    capitulo 5

--- Ejercicio23.java
+++ Ejercicio23.java
@@ -6,6 +6,24 @@ import java.util.Scanner;
 public class Ejercicio23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        double n = 0;
+        double numeroIntroducido;
+        double suma = 0;
+        
+        System.out.print("Vaya introduciendo números:");
+
+        do {
+            numeroIntroducido = Integer.parseInt(sc.nextLine());
+            suma+=numeroIntroducido;
+            n++;
+        } while (suma <=10000);
+    
+            
+        System.out.print("Has introducido un total de " + n + " numeros");
+        System.out.println("La suma es de " + suma);
+        System.out.println("La media es de " + (suma/numeroIntroducido));
+
         sc.close();
     }
 }
