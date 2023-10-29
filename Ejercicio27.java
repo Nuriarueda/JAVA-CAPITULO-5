e82ac9b28f58bad0b422dbcaefa8ad6f6d1619b8
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:21:28 +0100

    capitulo 5

--- Ejercicio27.java
+++ Ejercicio27.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 public class Ejercicio27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        int contador = 0 ;
+        int suma = 0;
+        System.out.print("Introduce un numero: ");
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 1; i<=n; i++){
+            if (i%3==0) {
+                System.out.println(i + " ");
+                contador++;
+                suma+=i;
+            }
+        }
+        System.out.println("Desde 1 hasta " + n + " hay " + contador );
+        System.out.println("La suma es de " + suma);
         sc.close();
     }
 }
