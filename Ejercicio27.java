031750a0e48e28cdc260992fc87376fb3af98755
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 13:10:44 +0100

    capitulo 5

---
+++ Ejercicio27.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que muestre, cuente y sume los múltiplos de 3 que hay
+entre 1 y un número leído por teclado. */
+import java.util.Scanner;
+public class Ejercicio27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
