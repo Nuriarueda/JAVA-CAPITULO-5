f5652dd29f7dcaf01b18f11a80d5a037ff463f53
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-29 12:53:10 +0100

    capitulo 5

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Ejercicio17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero: ");
+        int n = sc.nextInt();
+        int suma = 0;
+
+        if (n>0) {
+            for (int i = n; i<n+100; i++){
+                suma += i;
+            }
+            System.out.println("La suma de los numeros es " + suma);
+        } else {
+            System.out.println("El numero introducido no es positivo");
+        }
+
         sc.close();
     }
 }
