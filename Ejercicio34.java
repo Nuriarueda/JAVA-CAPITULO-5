fe1d6b351be0b21cd14052615f1c1686aa9fcf30
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 10:22:59 +0100

    capitulo 5

---
+++ Ejercicio34.java
@@ -0,0 +1,15 @@
+/* Escribe un programa que pida dos números por teclado y que luego mezcle en
+dos números diferentes los dígitos pares y los impares. Se van comprobando los
+dígitos de la siguiente manera: primer dígito del primer número, primer dígito
+del segundo número, segundo dígito del primer número, segundo dígito del
+segundo número, tercer dígito del primer número… Para facilitar el ejercicio,
+podemos suponer que el usuario introducirá dos números de la misma longitud
+y que siempre habrá al menos un dígito par y uno impar. Usa long en lugar de
+int donde sea necesario para admitir números largos. */
+import java.util.Scanner;
+public class Ejercicio34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
