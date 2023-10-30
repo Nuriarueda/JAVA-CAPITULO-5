d4f291939e4c484eb7e1c7f9f2bd4c7d8239f380
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 10:22:01 +0100

    capitulo 5

---
+++ Ejercicio33.java
@@ -0,0 +1,11 @@
+/* Realiza un programa que pinte la letra U por pantalla hecha con asteriscos. El
+programa pedirá la altura. Fíjate que el programa inserta un espacio y pinta
+dos asteriscos menos en la base para simular la curvatura de las esquinas
+inferiores. */
+import java.util.Scanner;
+public class Ejercicio33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
