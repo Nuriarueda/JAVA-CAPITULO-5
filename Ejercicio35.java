629500aa99eac038bbbaf7b631f082ecbc433cb2
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 10:40:13 +0100

    capitulo 5

---
+++ Ejercicio35.java
@@ -0,0 +1,11 @@
+/* Realiza un programa que pinte una X hecha de asteriscos. El programa debe
+pedir la altura. Se debe comprobar que la altura sea un número impar mayor o
+igual a 3, en caso contrario se debe mostrar un mensaje de error. */
+import java.util.Scanner;
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Introduzca la altura de la x: ");
+        sc.close();
+    }
+}
