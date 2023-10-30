579946f19502b40fae5db663226dad11886f7718
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 08:48:23 +0100

    capitulo 5

--- Ejercicio28.java
+++ Ejercicio28.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 public class Ejercicio28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un numero del que quieras saber el factorial: ");
+        int num = Integer.parseInt(sc.nextLine());
+        int fact = num;
+
+        for(int i = 1; i < num; i++ ){
+            fact *= i;
+        }
+
+        System.out.println(num + "! = " + fact);
         sc.close();
     }
 }
