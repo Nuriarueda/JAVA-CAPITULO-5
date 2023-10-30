fe6f34a98414ea274f558059abcdb9e7797d1881
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 09:13:50 +0100

    capitulo 5

--- Ejercicio29.java
+++ Ejercicio29.java
@@ -6,7 +6,18 @@ public class Ejercicio29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Introduce un numero: ");
+        int numIntroducido = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Introduce otro numero: ");
+        int divisibles = Integer.parseInt(sc.nextLine());
+
         
+        for (int i = numIntroducido; i > 2; i--){
+            if (i%divisibles!=0) {
+                System.out.print("Los numero que no son divisiblees entre " + divisibles + " son = " + i + " ");
+            }
+        }
         sc.close();
     }
 }
