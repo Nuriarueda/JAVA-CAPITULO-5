be5f66347660376c2c674e7184b10c9046b373b8
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 08:38:51 +0100

    capitulo 5

---
+++ Ejercicio29.java
@@ -0,0 +1,10 @@
+/* Escribe un programa que muestre por pantalla todos los números enteros
+positivos menores a uno leído por teclado que no sean divisibles entre otro
+también leído de igual forma. */
+import java.util.Scanner;
+public class Ejercicio29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
