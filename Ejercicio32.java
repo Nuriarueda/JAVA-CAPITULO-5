186c5dd9fc951a3f64758abce5e34ac894ab6a93
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 10:20:47 +0100

    capitulo 5

---
+++ Ejercicio32.java
@@ -0,0 +1,11 @@
+/* Escribe un programa que, dado un número entero positivo, diga cuáles son y
+cuánto suman los dígitos pares. Los dígitos pares se deben mostrar en orden, de
+izquierda a derecha. Usa long en lugar de int donde sea necesario para admitir
+números largos. */
+import java.util.Scanner;
+public class Ejercicio32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
