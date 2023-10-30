2c98f020b950bbf2985064ef4d578a772f0c58a3
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-30 08:37:33 +0100

    capitulo 5

--- Ejercicio28.java
+++ Ejercicio28.java
@@ -1,3 +1,8 @@
+/* Escribe un programa que calcule el factorial de un número entero leído por
+teclado. */
+import java.util.Scanner;
 public class Ejercicio28 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
