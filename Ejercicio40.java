e8866279dff7a3c3b90c962ec7cde47558392f38
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:21:24 +0100

    capitulo 5

--- Ejercicio40.java
+++ Ejercicio40.java
@@ -1,3 +1,10 @@
+/* Realiza un programa que pinte por pantalla un rombo hueco hecho con
+asteriscos. El programa debe pedir la altura. Se debe comprobar que la altura
+sea un número impar mayor o igual a 3, en caso contrario se debe mostrar un
+mensaje de error. */
+import java.util.Scanner;
 public class Ejercicio40 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
