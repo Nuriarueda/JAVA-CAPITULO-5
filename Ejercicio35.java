9f39182d96632362ee26ac81a7eccfd2a183bd49
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:12:54 +0100

    capitulo 5

--- Ejercicio35.java
+++ Ejercicio35.java
@@ -5,7 +5,52 @@ import java.util.Scanner;
 public class Ejercicio35 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Introduzca la altura de la x: ");
+        System.out.print("Por favor, introduzca la altura de la X: ");
+        int alturaIntroducida = Integer.parseInt(sc.nextLine());
+        
+        int altura = 1;
+        int i = 0;
+        int espaciosInternos = alturaIntroducida - 1;
+        int espaciosPorDelante = 0;
+
+        if ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
+            System.out.print("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
+        } else {
+            while (altura < alturaIntroducida / 2 + 1) {
+                for (i = 1; i <= espaciosPorDelante; i++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                for (i = 1; i < espaciosInternos; i++) {
+                    System.out.print(" ");
+                }       
+
+                System.out.print("*");
+                System.out.println();
+                altura++;
+                espaciosPorDelante++;
+                espaciosInternos -= 2;
+            } 
+            espaciosInternos = 0;
+            espaciosPorDelante = alturaIntroducida / 2;
+            altura = 1;
+            while (altura <= alturaIntroducida / 2 + 1) {
+                for (i = 1; i <= espaciosPorDelante; i++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                for (i = 1; i < espaciosInternos; i++) {
+                    System.out.print(" ");
+                }
+                if(altura>1) {
+                    System.out.print("*");
+                }
+                System.out.println();
+                altura++;
+                espaciosPorDelante--;
+                espaciosInternos+=2;
+            } 
+        } 
         sc.close();
     }
 }
