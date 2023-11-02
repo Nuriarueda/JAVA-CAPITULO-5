4f49ebdb9f88c2d1a486e2b306a954c656d66816
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:13:58 +0100

    capitulo 5

--- Ejercicio36.java
+++ Ejercicio36.java
@@ -1,3 +1,10 @@
+/* Realiza un programa que pida primero un número y a continuación un dígito.
+El programa nos debe dar la posición (o posiciones) contando de izquierda a
+derecha que ocupa ese dígito en el número introducido. */
+import java.util.Scanner;
 public class Ejercicio36 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
