56e4048a370d480123b1ec1035ecd6e824585601
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:19:22 +0100

    capitulo 5

--- Ejercicio38.java
+++ Ejercicio38.java
@@ -1,3 +1,7 @@
+/* Realiza un programa que pinte un reloj de arena relleno hecho de asteriscos. El
+programa debe pedir la altura. Se debe comprobar que la altura sea un número
+impar mayor o igual a 3, en caso contrario se debe mostrar un mensaje de error. */
+import java.util.Scanner;
 public class Ejercicio38 {
     
 }
