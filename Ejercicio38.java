fcb0ae44a82853d6c11a7438be1f2ef2a08034d2
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:19:41 +0100

    capitulo 5

--- Ejercicio38.java
+++ Ejercicio38.java
@@ -3,5 +3,8 @@ programa debe pedir la altura. Se debe comprobar que la altura sea un número
 impar mayor o igual a 3, en caso contrario se debe mostrar un mensaje de error. */
 import java.util.Scanner;
 public class Ejercicio38 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
