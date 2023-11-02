a9a69a7b4538ab7fe6523fb9699fb8ac03a22158
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 09:36:40 +0100

    capitulo 5

--- Ejercicio31.java
+++ Ejercicio31.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Ejercicio31 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduzca la altura de la L: ");
+        int altura = Integer.parseInt(sc.nextLine());
+
+        for (int i = 1; i < altura; i++) {
+            System.out.println("*");
+        }
+
+        for (int i = 0; i < altura / 2 + 1; i++) {
+            System.out.print("* ");
+        }
         sc.close();
     }
 }
