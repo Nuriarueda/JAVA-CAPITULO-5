901c85216a82164ad388bd55ec7293ce713832ed
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 09:38:43 +0100

    capitulo 5

--- Ejercicio33.java
+++ Ejercicio33.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Ejercicio33 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        
         sc.close();
     }
 }
