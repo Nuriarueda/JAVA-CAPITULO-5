4cdb4409644dd4c91b4036339925f3d6319ff86d
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:20:26 +0100

    capitulo 5

--- Ejercicio39.java
+++ Ejercicio39.java
@@ -1,3 +1,9 @@
+/* Escribe un programa que pida un número entero positivo por teclado y que
+muestre a continuación los números desde el 1 al número introducido junto
+con su factorial. */
+import java.util.Scanner;
 public class Ejercicio39 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
