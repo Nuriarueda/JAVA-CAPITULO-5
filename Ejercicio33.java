f0204f725f90533711fd731b19ac94a11f0937e3
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 09:39:34 +0100

    capitulo 5

--- Ejercicio33.java
+++ Ejercicio33.java
@@ -7,7 +7,15 @@ public class Ejercicio33 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        
+        System.out.print("Introduzca la altura de la U: ");
+        int altura = Integer.parseInt(sc.nextLine());
+
+        for (int i = 1; i < altura; i++) {
+            System.out.print("* ");
+            for (int j = 2; j < altura; j++) {
+                System.out.print(" ");
+            }
+        }
         sc.close();
     }
 }
