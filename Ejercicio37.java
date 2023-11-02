1a5f4dad6d7885cdb8d627a664daebfc341d4bbd
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-02 10:18:27 +0100

    capitulo 5

--- Ejercicio37.java
+++ Ejercicio37.java
@@ -1,5 +1,7 @@
-/*  */
+/* Realiza un conversor del sistema decimal al sistema de “palotes”. */
 import java.util.Scanner;
 public class Ejercicio37 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
