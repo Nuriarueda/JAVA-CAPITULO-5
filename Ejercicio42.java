9f8e11495aaaba519f87cd0dff8dc833761ea5c7
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 08:44:42 +0100

    capitulo 5

--- Ejercicio42.java
+++ Ejercicio42.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class Ejercicio42 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero: ");
+        int num = Integer.parseInt(sc.nextLine());
+        boolean esPrimo;
+
+        for (int i = num; i<=num+5; i++){
+
+            esPrimo = true;
+            for (int j = 2; j < i; j++){
+                if (i%j==0) {
+                    esPrimo = false;
+                }
+            }
+
+            if (esPrimo) {
+                System.out.println( i + " es primo");
+            } else {
+                System.out.println( i + " no es primo");
+            }
+        }
         sc.close();
     }
 }
