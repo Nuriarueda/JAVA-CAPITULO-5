506b8f799be60d2b630c3fa865d69dd69c8f1d7a
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 09:30:05 +0100

    capitulo 5

---
+++ Ejercicio45.java
@@ -0,0 +1,12 @@
+/* Escribe un programa que cambie un dígito dentro de un número dando la
+posición y el valor nuevo. Las posiciones se cuentan de izquierda a derecha
+empezando por el 1. Se recomienda usar long en lugar de int ya que el
+primero admite números más largos. Suponemos que el usuario introduce
+correctamente los datos. */
+import java.util.Scanner;
+public class Ejercicio45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
