81d10b2f42bd025bcbb9e40ad3b79365cbbe6587
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 08:21:07 +0100

    capitulo 5

---
+++ Ejercicio41.java
@@ -0,0 +1,10 @@
+/* Escribe un programa que diga cuántos dígitos pares y cuántos dígitos impares
+hay dentro de un número. Se recomienda usar long en lugar de int ya que el
+primero admite números más largos. */
+import java.util.Scanner;
+public class Ejercicio41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
