b7d2764951198237f4bf7dc6fef2fbbb92ed8eb8
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 08:36:06 +0100

    capitulo 5

--- Ejercicio41.java
+++ Ejercicio41.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Ejercicio41 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero: ");
+        long num = Long.parseLong(sc.nextLine());
+
+        for (int i = 0; i <= num; i++){
+            
+        }
         sc.close();
     }
 }
---
+++ Ejercicio42.java
@@ -0,0 +1,10 @@
+/* muestre a continuación los 5 números consecutivos a partir del número
+introducido. Al lado de cada número se debe indicar si se trata de un primo
+o no. */
+import java.util.Scanner;
+public class Ejercicio42 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
