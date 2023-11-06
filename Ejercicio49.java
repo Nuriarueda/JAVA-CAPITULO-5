356d24fc00bf267f5fb00955101a70235e4eb635
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 09:46:00 +0100

    capitulo 5

--- Ejercicio49.java
+++ Ejercicio49.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
 public class Ejercicio49 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
