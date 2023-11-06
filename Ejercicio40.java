91f19c3949d65a040af1cab367571be304d7d017
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-06 08:31:54 +0100

    capitulo 5

--- Ejercicio40.java
+++ Ejercicio40.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Ejercicio40 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce la altura del rombo: ");
+        int altura = Integer.parseInt(sc.nextLine());
+
+        for (int i = 1; i <= altura; i++){
+            for (int j = 1; j <= altura; j++){
+                
+            }
+        }
+
         sc.close();
     }
 }
