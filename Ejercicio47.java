b619caca2daf1439ec1542438bf7b56acecb3dd4
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-08 12:12:39 +0100

    capitulo 5

--- Ejercicio47.java
+++ Ejercicio47.java
@@ -1,3 +1,9 @@
+/* Con motivo de la celebración del día de la mujer, el 8 de marzo, nos han
+encargado realizar un programa que pinte un 8 por pantalla usando la letra
+M. Se pide al usuario la altura, que debe ser un número entero impar mayor
+o igual que 5. Si el número introducido no es correcto, el programa deberá
+mostrar un mensaje de error. A continuación se muestran algunos ejemplos.
+La anchura de la figura siempre será de 6 caracteres. */
 import java.util.Scanner;
 public class Ejercicio47 {
     public static void main(String[] args) {
