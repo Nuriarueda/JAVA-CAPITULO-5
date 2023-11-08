62294118457fe4b11f289d05530e2648b981a3c0
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-08 12:12:13 +0100

    capitulo 5

--- Ejercicio46.java
+++ Ejercicio46.java
@@ -1,3 +1,7 @@
+/* Realiza un programa que pinte por pantalla un rectángulo hueco hecho con
+asteriscos. Se debe pedir al usuario la anchura y la altura. Hay que comprobar
+que tanto la anchura como la altura sean mayores o iguales que 2, en caso
+contrario se debe mostrar un mensaje de error. */
 import java.util.Scanner;
 public class Ejercicio46 {
   public static void main(String[] args) {
--- Ejercicio49.java
+++ Ejercicio49.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Ejercicio49 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        
         sc.close();
     }
 }
