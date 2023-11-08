f45026663b42515e3f2d1e2d35438d7b67759079
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-08 12:13:39 +0100

    capitulo 5

--- Ejercicio49.java
+++ Ejercicio49.java
@@ -1,4 +1,8 @@
-/*  */
+/* Realiza un programa que calcule el máximo, el mínimo y la media de una serie
+de números enteros positivos introducidos por teclado. El programa terminará
+cuando el usuario introduzca un número primo. Este último número no se
+tendrá en cuenta en los cálculos. El programa debe indicar también cuántos
+números ha introducido el usuario (sin contar el primo que sirve para salir). */
 import java.util.Scanner;
 public class Ejercicio49 {
     public static void main(String[] args) {
