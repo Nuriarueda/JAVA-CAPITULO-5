f429b386b6110c8517df1182dd54f74f349bbee8
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-09 08:45:28 +0100

    capitulo 5

---
+++ Ejercicio53.java
@@ -0,0 +1,9 @@
+/* Realiza un programa que pinte un triángulo relleno tal como se muestra en los
+ejemplos. El usuario debe introducir la altura de la figura. */
+import java.util.Scanner;
+public class Ejercicio53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
