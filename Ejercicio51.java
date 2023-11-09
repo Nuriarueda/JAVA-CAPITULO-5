5b199f3460505ed44acffc15961f09919a2199a1
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-09 08:43:02 +0100

    capitulo 5

--- Ejercicio51.java
+++ Ejercicio51.java
@@ -1,3 +1,11 @@
+/* El gusano numérico se come los dígitos con forma de rosquilla, o sea, el 0 y el 8
+(todos los que encuentre). Realiza un programa que muestre un número antes
+y después de haber sido comido por el gusano. Si el animalito no se ha comido
+ningún dígito, el programa debe indicarlo. */
+import java.util.Scanner;
 public class Ejercicio51 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
