78543313096365c364b8373af5d533b189c5624d
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-09 08:47:00 +0100

    capitulo 5

--- Ejercicio54.java
+++ Ejercicio54.java
@@ -1,3 +1,9 @@
+/* Realiza un programa que pinte un triángulo hueco tal como se muestra en los
+ejemplos. El usuario debe introducir la altura de la figura. */
+import java.util.Scanner;
 public class Ejercicio54 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
