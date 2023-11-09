e94878500b1dde0b268f35b05f98537d2be1ca75
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-09 08:44:12 +0100

    capitulo 5

--- Ejercicio52.java
+++ Ejercicio52.java
@@ -1,3 +1,11 @@
+/* Realiza un programa que sea capaz de desplazar todos los dígitos de un número
+de derecha a izquierda una posición. El dígito de más a la izquierda, pasaría a
+dar la vuelta y se colocaría a la derecha. Si el número tiene un solo dígito, se
+queda igual. */
+import java.util.Scanner;
 public class Ejercicio52 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
