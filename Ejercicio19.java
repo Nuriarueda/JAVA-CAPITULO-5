d74a916da9cc3580696f1f86f2e532de17c3998e
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-11 18:51:29 +0100

    capitulo 5

--- Ejercicio19.java
+++ Ejercicio19.java
@@ -6,12 +6,21 @@ public class Ejercicio19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        System.out.print("Introduce la altura de la piramide por teclado: ");
-        int alt = Integer.parseInt(sc.nextLine());
-        System.out.print("Introduce el caracter con el que se pintara la piramide: ");
-        String caracter = sc.nextLine();
-
+        System.out.print("De que altura quieres la piramide: ");
+        int altura = sc.nextInt();
         
+        System.out.print("Que caracter quieres utilizar para hacer la piramide: ");
+        String caracter = sc.next();
+        
+        for (int i = 1; i <= altura; i++) {
+            for (int j = i; j <altura ; j++){
+                System.out.print(" "); 
+            }
+            for (int k = 1; k < i*2 ; k++){
+                System.out.print(caracter); 
+            }
+            System.out.println(" ");
+        }
         sc.close();
     }
 }
